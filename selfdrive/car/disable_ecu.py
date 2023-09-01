1da55997d8d41ab20c08efa6f5971daa3f35f4fb
Caleb Stultz <hi@calebstultz.dev>
2023-08-31 22:12:57 -0700

    Enable debug on disable_ecu
    - Setup response address for Communication Control UDS service (0x28)

--- selfdrive/car/disable_ecu.py
+++ selfdrive/car/disable_ecu.py
@@ -4,10 +4,10 @@ from openpilot.system.swaglog import cloudlog
 EXT_DIAG_REQUEST = b'\x10\x03'
 EXT_DIAG_RESPONSE = b'\x50\x03'
 
-COM_CONT_RESPONSE = b''
+COM_CONT_RESPONSE = b'\x68\x03\x01'
 
 
-def disable_ecu(logcan, sendcan, bus=0, addr=0x7d0, sub_addr=None, com_cont_req=b'\x28\x83\x01', timeout=0.1, retry=10, debug=False):
+def disable_ecu(logcan, sendcan, bus=0, addr=0x7d0, sub_addr=None, com_cont_req=b'\x28\x03\x01', timeout=0.1, retry=10, debug=True):
   """Silence an ECU by disabling sending and receiving messages using UDS 0x28.
   The ECU will stay silent as long as openpilot keeps sending Tester Present.
 
